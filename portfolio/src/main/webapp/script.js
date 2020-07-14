098aff73d426e122373b46271e8c1e2a3531e4e8
Arijit Saha <arijitbiley@gmail.com>
2020-07-14 18:17:42 +0530

    Add JS logic to load comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,7 +1,28 @@
-function getData() {
-  fetch('/data').then(response => response.text()).then(data => {
-    document.getElementById('fetch_info').innerText = data;
+/* Constants */
+const commentDivClassName = 'commentDiv';
+const commentUnorderedListClassName = 'commentList';
+const commentListItemClassName = 'commentItem';
+
+/*
+ * Function to fetch comments from the servlet
+ */
+function getComments() {
+  fetch('/comments').then(response => response.json()).then(comments => {
+    addCommentsToDOM(comments);
   });
+}
+
+/*
+ * Function to add comment elements to the DOM
+ */
+function addCommentsToDOM(comments) {
+  let listHTML = '';
+  for (comment of comments) {
+    listHTML += '<li class=\"' + commentListItemClassName
+                + '\">' + comment + '</li>';
+  }
+  document.querySelector('ul.' + commentUnorderedListClassName)
+          .innerHTML = listHTML;
 }
 
 /*
@@ -9,5 +30,5 @@ function getData() {
  * the window has loaded
  */
 window.onload = () => {
-  getData();
+  getComments();
 }

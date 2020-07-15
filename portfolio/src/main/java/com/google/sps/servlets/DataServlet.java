9609b8ff624d1fbff0fc4b400b742db02977cd13
Arijit Saha <arijitbiley@gmail.com>
2020-07-15 14:23:36 +0530

    Load comments from datastore

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -17,6 +17,9 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -32,13 +35,24 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Convert comments data to JSON
-    /* Gson gson = new Gson();
-    String json = gson.toJson(comments);
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.ASCENDING);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    List<String> comments = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+      long id = entity.getKey().getId();
+      String commentText = (String) entity.getProperty("text");
+      long timestamp = (long) entity.getProperty("timestamp");
+
+      comments.add(commentText);
+    }
 
     // Send the JSON as the response
+    Gson gson = new Gson();
     response.setContentType("application/json;");
-    response.getWriter().println(json); */
+    response.getWriter().println(gson.toJson(comments));
   }
 
   @Override

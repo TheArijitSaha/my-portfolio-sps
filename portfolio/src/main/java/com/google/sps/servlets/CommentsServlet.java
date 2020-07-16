cb3908c805c99a9c67411c97d9a476566b52e21a
Arijit Saha <arijitbiley@gmail.com>
2020-07-16 19:08:17 +0530

    Renamed DataServlet to CommentsServlet for appropriateness

--- portfolio/src/main/java/com/google/sps/servlets/CommentsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CommentsServlet.java
@@ -31,7 +31,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /* Servlet that returns comments data */
 @WebServlet("/comments")
-public class DataServlet extends HttpServlet {
+public class CommentsServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {

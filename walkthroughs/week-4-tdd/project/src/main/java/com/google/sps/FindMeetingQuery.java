bc88efeb9be302f162ce2d661e6bbc17691bc8f1
Arijit Saha <arijitbiley@gmail.com>
2020-07-18 23:25:15 +0530

    Extract available intervals that can be used to hold a meeting of required duration from the list of busy intervals given

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,39 +14,99 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 
 public final class FindMeetingQuery {
+
+  /**
+   * Returns a list of feasible intervals to hold a meeting which matches
+   * contraints in given in request and has no overlap with the list of events
+   * provided
+   *
+   * @param events A list of Events registered on a single day
+   * @param request A MeetingRequest containing details of the meeting
+   */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    HashSet<TimeRange> busyTimeRanges =
-      getBusyTimeRanges(events,
+    List<TimeRange> busyIntervals =
+      getBusyIntervals(events,
                         request.getAttendees());
-    // sort time ranges
-    Collection<TimeRange> availableTimeRanges =
-      getAvailableTimeRanges(busyTimeRanges,
+
+    Collection<TimeRange> availableIntervals =
+      getAvailableIntervals(busyIntervals,
                              request.getDuration());
-    return availableTimeRanges;
+
+    return availableIntervals;
   }
 
-  private static HashSet<TimeRange> getBusyTimeRanges(
+  /**
+   * Returns a list of intervals in which at least one of the required
+   * attendees of the meeting are unavailable
+   *
+   * @param events A list of Events registered on a single day
+   * @param requiredAttendees A list of people required to attend the new meeting
+   */
+  private static List<TimeRange> getBusyIntervals(
       Collection<Event> events,
       Collection<String> requiredAttendees) {
-    HashSet<TimeRange> busyTimeRanges = new HashSet<TimeRange>();
+    // A hashset, so that intervals are unique
+    HashSet<TimeRange> busyTimeRangesSet = new HashSet<TimeRange>();
 
     for (Event event: events) {
       if (!Collections.disjoint(event.getAttendees(), requiredAttendees)) {
-        busyTimeRanges.add(event.getWhen());
+        busyTimeRangesSet.add(event.getWhen());
       }
     }
 
-    return busyTimeRanges;
+    List<TimeRange> busyIntervals = new ArrayList<TimeRange>(busyTimeRangesSet);
+
+    return busyIntervals;
   }
 
-  private static Collection<TimeRange> getAvailableTimeRanges(
-      HashSet<TimeRange> busyTimeRanges,
+  /**
+   * Returns a list of intervals in which a meeting of required duration
+   * can be held where the required attendees are not busy otherwise
+   *
+   * @param busyIntervals A list of TimeRange each representing an interval where
+   *        at least one required attendee is unavailable
+   * @param duration An integer representing the duration of the meeting
+   */
+  private static Collection<TimeRange> getAvailableIntervals(
+      List<TimeRange> busyIntervals,
       long duration) {
-    return busyTimeRanges;
+    // currentOpening stores the minimum minute number of the day where a
+    // meeting can be started currently
+    int currentOpening = 0;
+    List<TimeRange> availableIntervals = new ArrayList<TimeRange>();
+
+    // If duration is non-positive, the meeting can be held any time
+    if (duration < 1) {
+      availableIntervals.add(TimeRange.WHOLE_DAY);
+      return availableIntervals;
+    }
+
+    Collections.sort(busyIntervals, TimeRange.ORDER_BY_START);
+
+    for (TimeRange busyInterval: busyIntervals) {
+      // If there is an interval big enough to accomodate a meeting of required
+      // duration then add the time interval to the list
+      if (currentOpening + duration <= busyInterval.start()) {
+        availableIntervals.add(
+          TimeRange.fromStartEnd(currentOpening, busyInterval.start(), false));
+      }
+
+      currentOpening = Math.max(currentOpening, busyInterval.end());
+    }
+
+    // If enough time is left at the end of day to accomodate a meeting, add that
+    if (currentOpening + duration <= TimeRange.END_OF_DAY) {
+      availableIntervals.add(
+        TimeRange.fromStartEnd(currentOpening, TimeRange.END_OF_DAY, true));
+    }
+
+    return availableIntervals;
   }
 }
